24c61507a74689ee91f7b856876765e9c628968c
Cláudio Aguiar <claudioaguiargomes@gmail.com>
2023-05-15 21:15:20 -0400

    add

---
+++ main.py
@@ -0,0 +1,38 @@
+from customtkinter import *
+from tkinter import CENTER, BOTTOM, LEFT, RIGHT, TOP
+from pyautogui import position
+
+class App:
+    def __init__(self):
+        self.root = CTk()
+        self.root.geometry('300x200')
+        self.root.title('Mouse Plot')
+        self.root.attributes("-topmost", False)
+        self.main()
+        self.root.mainloop()
+
+
+    def main(self):
+
+        def mouse_plot():
+            x, y = position()
+
+        self.frame_axis = CTkFrame(self.root)
+        self.frame_axis.pack()
+        self.label_x = CTkLabel(self.frame_axis, text='Position X')
+        self.label_x.pack()
+        self.label_y = CTkLabel(self.frame_axis, text='Position y')
+        self.label_y.pack()
+        self.switch_top = CTkSwitch(self.root, text='Always on Top', command=self.always_on_top)
+        self.switch_top.pack(side = BOTTOM)
+
+
+    def always_on_top(self):
+        option = self.switch_top.get()
+        if option == 1:
+            self.root.attributes("-topmost", True)
+        else:
+            self.root.attributes("-topmost", False)
+
+
+App()
